0de1a29e3e8da2c90915fbb3e61df366d9cdb633
zhangpetergo <zhangpetergo@gmail.com>
2025-03-23 21:32:36 +0800

    update: 更新 todo

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,15 @@ import (
 	"os"
 )
 
+/*
+ TODO: 1. 使用 ffmpeg 下载直播流
+	   2. 打印当前录制的直播流的信息
+	   3. 读取 url 的配置文件，支持热更新
+	   4. 支持多个 url 同时录制
+	   5. 支持对录制的配置
+
+*/
+
 func main() {
 	// 初始化日志
 	logger.InitLogger()
